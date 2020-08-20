78dd0bafd58d9b4cbbec593816ece19b475935d5
Ashlea Walker <ashleawalker29@gmail.com>
2020-08-20 12:17:18 -0600

    File Revisions
    - Added less dependent path call for the CSV.
        - Changed the name of the CSV itself as it was changed in #64.
    - Renamed variables to be more readable.
    - Changed chart output to generate and save an HTML file within the `bar_chart` directory.

--- assignments/bar_chart/bar_chart.py
+++ assignments/bar_chart/bar_chart.py
@@ -1,19 +1,14 @@
-"""
-This module is intended to create a Bar Chart from a .csv file.
-Author: Ashlea Walker
-"""
-import altair
-from pandas import read_csv
 import os
 
-os.chdir('D:\\Documents\\CS4770_datasets\\')
+import altair
+from pandas import read_csv
 
-dataset = read_csv('Video_Games_Sales_as_at_22_Dec_2016.csv')
+dataset = read_csv(os.path.abspath('../../csv/video_games_sales_as_of_22_Dec_2016.csv'))
 
 nintendo_data = dataset[dataset['Publisher'] == 'Nintendo']
 games_per_platform = nintendo_data.groupby('Platform').count().reset_index()
 
-game_chart = altair.Chart(games_per_platform).mark_bar(size = 30).encode(
+nintendo_platform_chart = altair.Chart(games_per_platform).mark_bar(size = 30).encode(
     altair.X('Platform:N', axis = altair.Axis(title='Platform')),
     altair.Y('Name:Q', axis = altair.Axis(title='Number of Games')),
     altair.Color('Platform:N', scale = altair.Scale(range=['#d20014']), legend = None),
@@ -23,7 +18,7 @@ game_chart = altair.Chart(games_per_platform).mark_bar(size = 30).encode(
     title='Unique Games Nintendo Published per Nintendo Platform as of December 2016'
 )
 
-game_chart_text = game_chart.mark_text(
+nintendo_platform_chart_text = nintendo_platform_chart.mark_text(
     baseline =  'middle',
     color = 'black',
     dy = -7
@@ -32,4 +27,6 @@ game_chart_text = game_chart.mark_text(
     opacity = altair.value(3.0)
 )
 
-(game_chart + game_chart_text)
+full_bar_chart = nintendo_platform_chart + nintendo_platform_chart_text
+
+full_bar_chart.save('bar_chart.html')
